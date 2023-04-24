224b453debee2f34f0d709d33fbc8b3ceb449d0b
ryhorowitz <ryan.w.horowitz@gmail.com>
2023-04-24 18:02:27 -0400

    Front and Back ends are hooked up. useEffect is rendering message from the server.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 function App() {
   // on load fetch message from BE
   const [message, setMessage] = useState('Hello, World!')
+
+  useEffect(() => {
+    fetch('http://localhost:9292/')
+      .then(r => r.json())
+      .then(data => setMessage(data.message))
+
+    return () => {
+
+    }
+  }, [])
+
   return (
     <div className="App">
       {message}
