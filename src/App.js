a82abb8bcb9af4e19e6280e8f248ebbdf292ab55
ryhorowitz <ryan.w.horowitz@gmail.com>
2023-04-24 17:56:21 -0400

    added state to render Hello, World!

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-
+import React, { useState } from "react";
 function App() {
+  // on load fetch message from BE
+  const [message, setMessage] = useState('Hello, World!')
   return (
     <div className="App">
-      Hello, World!
+      {message}
     </div>
   );
 }
