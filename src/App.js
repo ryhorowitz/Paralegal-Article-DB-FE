406d4c296a0a5a7a91436ea391da3b93a0ebaf0c
ryhorowitz <ryan.w.horowitz@gmail.com>
2023-04-25 22:05:30 -0400

    added react-table dep

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     fetch('http://localhost:9292/')
       .then(r => r.json())
       .then(data =>{
-        console.log(Array.isArray(data))
+        // console.log(Array.isArray(data))
         setArticles(data)
       })
   }, [])
