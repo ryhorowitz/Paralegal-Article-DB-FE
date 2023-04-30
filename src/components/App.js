e50459ff2f7da7dc1d47ac41b2704fa6277160f2
ryhorowitz <ryan.w.horowitz@gmail.com>
2023-04-30 18:17:03 -0400

    added some tailwinds styling

--- src/components/App.js
+++ src/components/App.js
@@ -9,16 +9,25 @@ function App() {
   useEffect(() => {
     fetch('http://localhost:9292/')
       .then(r => r.json())
-      .then(data =>{
+      .then(data => {
         // console.log(Array.isArray(data))
         setArticles(data)
       })
   }, [])
-  
+
   return (
     <div className="App">
-      <Header/>
-      <Table articles={articles}/>
+      <Header />
+      <div className="min-h-screen bg-gray-100 text-gray-900">
+        <main className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 pt-4">
+          <div className="">
+            <h1 className="text-xl font-semibold">Articles used by Paralegals</h1>
+          </div>
+          <div className="mt-4">
+            <Table articles={articles} />
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
--- src/components/Table.js
+++ src/components/Table.js
@@ -24,7 +24,7 @@ function Table({ articles }) {
 
   return (
 
-    <div className="mt-2 flex flex-col">
+    <div className="mt-5 flex flex-col">
       <div className="-my-2 overflow-x-auto -mx-4 sm:-mx-6 lg:-mx-8">
         <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
           <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
