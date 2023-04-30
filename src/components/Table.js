237ec76998438c1855425c4f87777ec2ca9644e4
ryhorowitz <ryan.w.horowitz@gmail.com>
2023-04-30 17:23:36 -0400

    react table loading dynamic data. error was because I had capitalized accessor.

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,21 +2,21 @@ import { useMemo } from 'react'
 import { useTable } from 'react-table'
 
 function Table({ articles }) {
-  
-  // const articleData = useMemo(() => articles, [articles])
+
+  const articleData = useMemo(() => articles, [articles])
   // console.log('1st 5 memoized', articleData)
-  // const articleColumns = useMemo(
-  //   () =>
-  //     articleData[0] ?
-  //       Object.keys(articleData)
-  //         .map(key => {
-  //           return {
-  //             Header: key,
-  //             Accessor: key
-  //           }
-  //         }) :
-  //       []
-  //   , [articleData])
+  const articleColumns = useMemo(
+    () =>
+      articleData[0] ?
+        Object.keys(articleData[0])
+          .map(key => {
+            return {
+              Header: key,
+              accessor: key
+            }
+          }) :
+        []
+    , [articleData])
 
 
   const dummyData = useMemo(
@@ -26,7 +26,7 @@ function Table({ articles }) {
         title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
         price: 109.95,
         description:
-        "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+          "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
         category: "men's clothing",
         image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
         rating: {
@@ -39,7 +39,7 @@ function Table({ articles }) {
         title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
         price: 109.95,
         description:
-        "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+          "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
         category: "men's clothing",
         image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
         rating: {
@@ -82,48 +82,49 @@ function Table({ articles }) {
     []
   );
 
-  const tableInstance = useTable({ columns: dummyColumns, data: dummyData })
+  const tableInstance = useTable({ columns: articleColumns, data: articleData })
 
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = tableInstance
 
   return (
-    < table {...getTableBodyProps()}>
+    < table {...getTableProps()}>
       <thead>
         {headerGroups.map(headerGroup => (
-         // Apply the header row props
-         <tr {...headerGroup.getHeaderGroupProps()}>
-           {// Loop over the headers in each row
-           headerGroup.headers.map(column => (
-             // Apply the header cell props
-             <th {...column.getHeaderProps()}>
-               {// Render the header
-               column.render('Header')}
-             </th>
-           ))}
-         </tr>
-       ))}
+          // Apply the header row props
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {// Loop over the headers in each row
+              headerGroup.headers.map(column => (
+                // Apply the header cell props
+                <th {...column.getHeaderProps()}>
+                  {// Render the header
+                    column.render('Header')}
+                </th>
+              ))}
+          </tr>
+        ))}
       </thead>
       <tbody {...getTableBodyProps()}>
-         {// Loop over the table rows
-       rows.map(row => {
-        // Prepare the row for display
-        prepareRow(row)
-        return (
-          // Apply the row props
-          <tr {...row.getRowProps()}>
-            {// Loop over the rows cells
-            row.cells.map(cell => {
-              // Apply the cell props
-              return (
-                <td {...cell.getCellProps()}>
-                  {// Render the cell contents
-                  cell.render('Cell')}
-                </td>
-              )
-            })}
-          </tr>
-        )
-      })}
+        {// Loop over the table rows
+          rows.map(row => {
+            // Prepare the row for display
+            prepareRow(row)
+            return (
+              // Apply the row props
+              <tr {...row.getRowProps()}>
+                {// Loop over the rows cells
+                  row.cells.map(cell => {
+                    // Apply the cell props
+                    return (
+                      <td {...cell.getCellProps()}>
+                        {console.log('cell is', cell)}
+                        {// Render the cell contents
+                          cell.render('Cell')}
+                      </td>
+                    )
+                  })}
+              </tr>
+            )
+          })}
       </tbody>
     </table>
 
