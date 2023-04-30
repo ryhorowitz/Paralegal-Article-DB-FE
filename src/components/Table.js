ef3ebb525cd003d62be30af9f0918f0e824b0884
ryhorowitz <ryan.w.horowitz@gmail.com>
2023-04-30 17:32:44 -0400

    added tailwinds for styling

--- src/components/Table.js
+++ src/components/Table.js
@@ -18,70 +18,6 @@ function Table({ articles }) {
         []
     , [articleData])
 
-
-  const dummyData = useMemo(
-    () => [
-      {
-        id: 1,
-        title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-        price: 109.95,
-        description:
-          "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-        category: "men's clothing",
-        image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-        rating: {
-          rate: 3.9,
-          count: 120,
-        },
-      },
-      {
-        id: 1,
-        title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-        price: 109.95,
-        description:
-          "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-        category: "men's clothing",
-        image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-        rating: {
-          rate: 3.9,
-          count: 120,
-        },
-      },
-      {
-        id: 1,
-        title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-        price: 109.95,
-        description:
-          "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-        category: "men's clothing",
-        image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-        rating: {
-          rate: 3.9,
-          count: 120,
-        },
-      },
-    ],
-    []
-  )
-
-  const dummyColumns = useMemo(
-    () => [
-      {
-        Header: "Id",
-        accessor: "id",
-      },
-      {
-        Header: "Price",
-        accessor: "price",
-      },
-      {
-        Header: "Title",
-        accessor: "title",
-      },
-    ],
-    []
-  );
-
   const tableInstance = useTable({ columns: articleColumns, data: articleData })
 
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = tableInstance
@@ -90,35 +26,24 @@ function Table({ articles }) {
     < table {...getTableProps()}>
       <thead>
         {headerGroups.map(headerGroup => (
-          // Apply the header row props
           <tr {...headerGroup.getHeaderGroupProps()}>
-            {// Loop over the headers in each row
-              headerGroup.headers.map(column => (
-                // Apply the header cell props
+            { headerGroup.headers.map(column => (
                 <th {...column.getHeaderProps()}>
-                  {// Render the header
-                    column.render('Header')}
+                  {column.render('Header')}
                 </th>
               ))}
           </tr>
         ))}
       </thead>
       <tbody {...getTableBodyProps()}>
-        {// Loop over the table rows
-          rows.map(row => {
-            // Prepare the row for display
+        {rows.map(row => {
             prepareRow(row)
             return (
-              // Apply the row props
               <tr {...row.getRowProps()}>
-                {// Loop over the rows cells
-                  row.cells.map(cell => {
-                    // Apply the cell props
+                {row.cells.map(cell => {
                     return (
                       <td {...cell.getCellProps()}>
-                        {console.log('cell is', cell)}
-                        {// Render the cell contents
-                          cell.render('Cell')}
+                        { cell.render('Cell')}
                       </td>
                     )
                   })}
